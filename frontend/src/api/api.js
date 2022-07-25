840ced0d6f8067c68a502a47427d317d81193192
Dimitrije <dsejat@gmail.com>
2022-07-25 11:22:51 +0200

    Route fixes

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,31 +1,28 @@
 import axios from "axios";
 
 const headers_default = () => {
-    return {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${atob(localStorage.token)}`,
-        Accept: "application/json",
-        "X-Api-Key": `adb69d232d124c98fe20400d9a4757d71380ba1d4200697e6817c99a30959ed2`,
-    };
+	return {
+		"Content-Type": "application/json",
+		Authorization: `Bearer ${atob(localStorage.token)}`,
+		Accept: "application/json",
+		"X-Api-Key": `adb69d232d124c98fe20400d9a4757d71380ba1d4200697e6817c99a30959ed2`,
+	};
 };
 
 export const api_axios = (method, query, data, headers = headers_default()) => {
-    // console.log(query);
-    return axios({
-        method: `${method}`,
-        url: `https://miljanpeles.com/renter_backend/public/api${query}`,
-        data: data,
-        headers: headers,
-    });
+	// console.log(query);
+	return axios({
+		method: `${method}`,
+		url: `http://localhost:5000/api${query}`,
+		data: data,
+		headers: headers,
+	});
 };
 
 export const params_axios = (query, params, headers = headers_default()) => {
-    // console.log(query);
-    return axios.get(
-        `https://miljanpeles.com/renter_backend/public/api${query}`,
-        {
-            params,
-            headers,
-        }
-    );
+	// console.log(query);
+	return axios.get(`http://localhost:5000/api${query}`, {
+		params,
+		headers,
+	});
 };
